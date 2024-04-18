3070f1ccebc7610e3bdf23cb395f729e89f22a6b
Cleiston Amaral <c.alexandresouza@yahoo.com>
2024-04-18 14:58:17 -0300

    installation of the token authentication

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,11 +1,30 @@
 class Api::V1::RestaurantsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_restaurant, only: [:show, :update]
+
   def index
     @restaurants = policy_scope(Restaurant)
   end
 
   def show
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render json: {message: 'se fudeu'}
+    end
+  end
+
+  private
+
+  def set_restaurant
     @restaurant = Restaurant.find(params[:id])
     authorize @restaurant
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
   end
 end
